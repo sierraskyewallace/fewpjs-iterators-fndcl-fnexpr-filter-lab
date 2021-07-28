91f3b188accf5c183b1efa6e5b23b06abe437221
sierraskyewallace <sskwallace@gmail.com>
2021-07-28 15:07:00 -0700

    done

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution here
+
+// findMatching() returns all drivers that match the passed in name 
+
+function findMatching(array,str) {
+    let matchingDrivers = array.filter( name => { return name.toLowerCase() === str.toLowerCase() })
+    return matchingDrivers 
+}
+
+/////////////////////////////////
+
+function fuzzyMatch(array,str) {
+    let matchingDrivers = array.filter( name => { return  name.startsWith(str)    })
+    return matchingDrivers
+
+}
+
+/////////////////////////////////
+
+function matchName(array,str) {
+    let matching = array.filter( driver => { return driver.name === str }    ) 
+    return matching 
+}
